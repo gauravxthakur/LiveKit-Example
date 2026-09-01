ea9c3a22fde46a1a99611f0c692b3fd8fd683558
gxt <gauravkthakur2002@gmail.com>
2026-09-01 18:08:31 +0530

    Install and add sarvam and cartesia plugins through get_tts_provider function

--- plugins/providers/providers.py
+++ plugins/providers/providers.py
@@ -1,5 +1,8 @@
-from livekit.agents import llm, stt
-from livekit.plugins import google, openai, deepgram, assemblyai
+from livekit.agents import llm, stt, tts
+from livekit.plugins import google, openai, deepgram, assemblyai, sarvam, cartesia
+
+
+#-------------------------------LLM----------------------------------------------
 
 def get_llm_provider(provider_type: str) -> llm.LLM:
     
@@ -12,6 +15,8 @@ def get_llm_provider(provider_type: str) -> llm.LLM:
     raise ValueError(f"Unknown LLM provider type: {provider_type}")
 
 
+#-------------------------------STT----------------------------------------------
+
 def get_stt_provider(provider_type: str = "deepgram") -> stt.STT:
 
     if provider_type == "deepgram":
@@ -21,3 +26,16 @@ def get_stt_provider(provider_type: str = "deepgram") -> stt.STT:
         return assemblyai.STT()
 
     raise ValueError(f"Unknown STT provider type: {provider_type}")
+
+
+#-------------------------------TTS----------------------------------------------
+
+def get_tts_provider(provider_type: str = "cartesia") -> tts.TTS:
+
+    if provider_type == "cartesia":
+        return cartesia.TTS(model="sonic-3", voice="a167e0f3-df7e-4d52-a9c3-f949145efdab")
+        
+    elif provider_type == "sarvam":
+        return sarvam.TTS(model="bulbul:v3", target_language_code="hi-IN", speaker="shubh")
+
+    raise ValueError(f"Unknown TTS provider type: {provider_type}")
